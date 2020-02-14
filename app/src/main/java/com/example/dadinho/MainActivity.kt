1053e74a597a8610c8cc9990388eb6de68997ce5
marcelo.vergara <marcelo.vergara@al.infnet.edu.br>
2020-02-14 12:02:33 -0300

    primeiro

---
+++ app/src/main/java/com/example/dadinho/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.dadinho
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn6.setOnClickListener{ rollDice(4) }
+
+        btn4.setOnClickListener{ rollDice(4) }
+
+        btn10.setOnClickListener{ rollDice(10) }
+
+        btn20.setOnClickListener{ rollDice(20) }
+        }
+
+    fun rollDice (num: Int){
+        val randomInt = Random.nextInt(num)+1
+        result.text = randomInt.toString()
+        val enum = randomInt.toString()
+        resultados.append("\t $enum \n")
+    }
+}
